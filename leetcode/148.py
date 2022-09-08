ad26159c5271479a324071efa6d6f16fed248293
jjin93 <tmdgus3901@gmail.com>
2022-09-08 21:15:46 +0900

    릿코드문제풀이

---
+++ leetcode/148.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def merge(self, h1, h2):
+        if not h1 or not h2:
+            return h1 or h2
+        dummy = tail = ListNode(None)
+        while h1 and h2:
+            if h1.val < h2.val:
+                tail.next = h1
+                h1 = h1.next
+            else:
+                tail.next = h2
+                h2 = h2.next
+            tail = tail.next
+        tail.next = h1 or h2
+        return dummy.next
+            
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        fast, slow = head.next, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        start = slow.next
+        slow.next = None
+        h1, h2 = self.sortList(head), self.sortList(start)
+        return self.merge(h1, h2)
